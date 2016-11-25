39751672cd88b84d04f5cd22576b05c45e9ddc28
Łukasz Kałużny <lukasz@kaluzny.pro>
2016-11-25 22:50:39 +0100

    Fix installVsCommunity2015.ps1

--- templates/installVsCommunity2015.ps1
+++ templates/installVsCommunity2015.ps1
@@ -2,5 +2,6 @@ $url = "https://lkdwhsrc.blob.core.windows.net/artifact-repository/vs2015.3.com_
 $output = "D:\vs2015.3.com_enu.iso"
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $output)
-Mount-DiskImage D:\vs2015.3.com_enu.iso
-Start-Process E:\vs_community.exe -Wait -ArgumentList /Passive,/Norestart
+$mountResult =  Mount-DiskImage D:\vs2015.3.com_enu.iso -PassThru
+$DriveLetter = ($mountResult | Get-Volume).DriveLetter
+Start-Process "${DriveLetter}:\vs_community.exe" -Wait -ArgumentList /Passive,/Norestart
