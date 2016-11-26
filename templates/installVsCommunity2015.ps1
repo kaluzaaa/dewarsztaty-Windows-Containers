92fc6b9c1bff40266275e388bf2404930fac5afb
Łukasz Kałużny <lukasz@kaluzny.pro>
2016-11-26 01:36:43 +0100

    Fix installVsCommunity2015.ps1

--- templates/installVsCommunity2015.ps1
+++ templates/installVsCommunity2015.ps1
@@ -1,7 +1,12 @@
+Write-Output "[$(Get-Date)] ========== Start =========="
 $url = "https://lkdwhsrc.blob.core.windows.net/artifact-repository/vs2015.3.com_enu.iso"
 $output = "D:\vs2015.3.com_enu.iso"
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($url, $output)
+Write-Output "[$(Get-Date)] File download"
+Invoke-WebRequest -Uri $url -OutFile $output
+Write-Output "[$(Get-Date)] File downloaded"
+Write-Output "[$(Get-Date)] Mount iso"
 $mountResult =  Mount-DiskImage D:\vs2015.3.com_enu.iso -PassThru
 $DriveLetter = ($mountResult | Get-Volume).DriveLetter
-Start-Process "${DriveLetter}:\vs_community.exe" -Wait -ArgumentList /Passive,/Norestart
+Write-Output "[$(Get-Date)] Start install"
+$cmd = Start-Process "${DriveLetter}:\vs_community.exe" -Wait -ArgumentList /q,/Norestart
+Write-Output "[$(Get-Date)] Install process exit code $($cmd.ExitCode)"
