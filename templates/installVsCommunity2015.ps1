1938c6421f4d47cec24f27474e52d851458575c6
Łukasz Kałużny <lukasz@kaluzny.pro>
2016-11-27 01:30:18 +0100

    Lab environment template

--- templates/installVsCommunity2015.ps1
+++ templates/installVsCommunity2015.ps1
@@ -9,5 +9,4 @@ Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss" )] Mount iso"
 $mountResult =  Mount-DiskImage D:\vs2015.3.com_enu.iso -PassThru
 $DriveLetter = ($mountResult | Get-Volume).DriveLetter
 Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss" )] Start install"
-$cmd = Start-Process "${DriveLetter}:\vs_community.exe" -ArgumentList /q,/Norestart,"/AdminFile ${DriveLetter}:\AdminDeployment.xml" -Wait -PassThru
-Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss" )] Install process exit code $($cmd.ExitCode)"
+$cmd = Start-Process "${DriveLetter}:\vs_community.exe" -ArgumentList /q,/Norestart,"/AdminFile ${DriveLetter}:\AdminDeployment.xml"
